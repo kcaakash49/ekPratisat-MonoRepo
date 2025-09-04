57c38fd258d9f7a991308d15f411f08456f98fb4
kcaakash49 <kcaakash4910@gmail.com>
2025-09-04 15:22:03 +0545

    init

--- apps/client-app/app/auth/signin/page.tsx
+++ apps/client-app/app/auth/signin/page.tsx
@@ -4,7 +4,7 @@ import { useSession } from "next-auth/react";
 
 export default function SignInPage(){
     const session = useSession();
-    console.log(session);
+    console.log(session.data?.user?.id);
 
     return <div>
         SiginInPage
