8e941249f14bfe506aefaeec7c3f30f55c7a3dbd
kcaakash49 <kcaakash4910@gmail.com>
2025-12-18 12:09:20 +0545

    init

---
+++ apps/admin-app/app/(pages)/loading.tsx
@@ -0,0 +1,10 @@
+
+
+import AnimateLoader from "@repo/ui/animateLoader";
+
+
+export default function Loading(){
+    return <div className="flex justify-center items-center h-full w-full">
+       <AnimateLoader/>
+    </div>
+}
---
+++ apps/admin-app/app/loading.tsx
@@ -0,0 +1,8 @@
+// import AnimateLoader from "@repo/ui/animateLoader";
+
+
+export default function Loading(){
+    return <div className="flex flex-1 justify-center items-center">
+       Loading.............
+    </div>
+}
