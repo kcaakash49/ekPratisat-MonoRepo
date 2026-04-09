0d4c2a171234dc98c9fb4234017f3c5b85336152
kcaakash49 <kcaakash4910@gmail.com>
2026-04-09 15:35:24 +0545

    recent listing complete

--- apps/client-app/app/(pages)/layout.tsx
+++ apps/client-app/app/(pages)/layout.tsx
@@ -4,7 +4,7 @@ import Navbar from "../../components/Navbar";
 
 export default function Layout({children} : {children: React.ReactNode}){
     return (
-        <div className="flex flex-col h-screen">
+        <div className="flex flex-col min-h-screen">
             <Navbar/>
             <main className="flex-1">
                 {children}
--- apps/client-app/app/(pages)/page.tsx
+++ apps/client-app/app/(pages)/page.tsx
@@ -1,13 +1,15 @@
 import { Categories } from "../../components/category/Categories";
-import Footer from "../../components/Footer";
 import HeroSection from "../../components/HeroSection";
-import VideoBackground from "../../components/VideoBackground";
+import RecentListings from "../../components/RecentListings";
+
+
 
 export default async function Home(){
   console.log("Rendering Home Page at", new Date().toISOString());
   return <div>
     <HeroSection/>
     <Categories/>
-    <Footer/>
+    <RecentListings/>
+   
   </div>
 }
