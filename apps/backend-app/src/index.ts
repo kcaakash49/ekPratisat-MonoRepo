1fc99955114afd67a2a3b35169ebe8afdffeb92a
kcaakash49 <kcaakash4910@gmail.com>
2026-04-10 18:03:02 +0545

    add json limit to express

--- apps/backend-app/src/index.ts
+++ apps/backend-app/src/index.ts
@@ -47,7 +47,8 @@ app.use(
 );
 
 app.use(cookieParser());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 app.use("/auth", authRouter);
 app.use("/listing", listingRouter);
